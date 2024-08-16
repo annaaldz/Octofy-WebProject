7bdbe5ffb7b2d6fec618cdd1ea109eec6baf5598
dayanesm <moreiradayane135@gmail.com>
2024-08-16 17:43:02 -0300

    seções de impacto e FAQ adicionadas

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,1 +1,15 @@
-import "../header_FREE/script.js"
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+// Adiciona um evento de clique para cada botão de pergunta
+faqQuestions.forEach(question => {
+    question.addEventListener('click', () => {
+        // Seleciona a resposta correspondente ao botão clicado
+        const answer = question.nextElementSibling;
+        // Alterna a visibilidade da resposta
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none'; // Oculta a resposta se ela estiver visível
+        } else {
+            answer.style.display = 'block'; // Exibe a resposta se ela estiver oculta
+        }
+    });
+});
